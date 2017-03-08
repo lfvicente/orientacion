b1b4226152f098f7a80ddaa48459bd945e9d8218
lfvicente <lfvicente@gmail.com>
2017-03-08 20:36:07 +0100

    Preparacion Pro

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(name: "Laura Manzaneque", email: "lamancar@gmail.com", rol: "orientador", password: "123456", password_confirmation: "123456")
+User.create(name: "Luis Vicente", email: "lfvicente@gmail.com", rol: "familia", password: "123456", password_confirmation: "123456")
